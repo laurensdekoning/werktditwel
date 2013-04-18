8852d7f30e13c3cf41555d542bed5d5c69192b33
Laurens de Koning <laurens@laurensdekoning.nl>
2013-04-18 22:01:15 +0200

    first commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public class Dog
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            // Constructor that takes individual property values
+            public Dog(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            // Copy constructor
+            public Dog(Dog otherDog)
+            {
+                Name = otherDog.Name;
+                Age = otherDog.Age;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Create a dog
+            Dog myDog = new Dog("Kirby", 15);
+
+            // Create a copy of Kirby
+            Dog other = new Dog(myDog);
+        }
+    }
+}
